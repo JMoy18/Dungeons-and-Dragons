6041099c7dc8c66d56a593bcb813aebe3c213421
JMoy18 <107206455+JMoy18@users.noreply.github.com>
2024-01-31 09:11:44 -0500

    In progress to add duplicate creatures - not done

--- initiativeTracker.py
+++ initiativeTracker.py
@@ -3,11 +3,12 @@ from flask import Flask, jsonify, request, render_template, request, redirect, u
 app = Flask(__name__)
 
 class Creature:
-    def __init__(self, name, initiative, health, armorClass, is_player=False,):
+    def __init__(self, name, initiative, health, armorClass, quantity = 1, is_player=False,):
         self.name = name
         self.initiative = initiative
         self.is_player = is_player
         self.health = health
+        self.quantity = quantity
         self.armorClass = armorClass
 
 creatures = []
@@ -15,6 +16,7 @@ players = []
 
 @app.route('/')
 def index():
+    #this sorts the creatures in 
     all_characters = sorted(creatures + players, key=lambda x: int(x.initiative), reverse=True)
     return render_template('index.html', characters=all_characters)
 
@@ -24,6 +26,7 @@ def add_creature():
     initiative = request.form.get('initiative')
     health = request.form.get('health')
     armorClass = request.form.get("armorClass")
+    quantity = request.form.get("quantity")
     is_player = request.form.get('is_player') == 'on'  # Checkbox value
 
     try:
@@ -33,13 +36,21 @@ def add_creature():
     except ValueError:
         # Validation failed, render the template with a warning message
         return render_template('index.html', characters=creatures + players, warning_message='Initiative/Health/AC must be a valid number')
+    
+    if quantity == '':
+        quantity = "1"
 
-    if is_player:
-        player = Creature(name, initiative, health, armorClass, is_player=True)
-        players.append(player)
-    else:
-        creature = Creature(name, initiative, health, armorClass)
-        creatures.append(creature)
+    numCreatures = int(quantity)
+
+    while numCreatures != 0:
+        if is_player:
+            player = Creature(name, initiative, health, armorClass, is_player=True)
+            players.append(player)
+            numCreatures -= 1
+        else:
+            creature = Creature(name, initiative, health, armorClass)
+            creatures.append(creature)
+            numCreatures -= 1
 
     return redirect(url_for('index'))
 
@@ -49,6 +60,7 @@ def update_character():
     property_name = request.form.get('property')
     value = request.form.get('value')
 
+    #something is broken here, in the case of multiple of the same name, we want to know which index it is at
     for character in creatures + players:
         if character.name == name:
             setattr(character, property_name, value)
@@ -67,6 +79,7 @@ def remove_selected():
     armor_class = int(selected_info_parts[3].split(': ')[1])
     is_player = selected_info_parts[4].split(': ')[1] == 'Yes'
 
+    #something is breaking if all is similar but AC, it does delete an item but it reverts the change back to the original value
     # Remove the character
     for character in creatures + players:
         if character.initiative == initiative and character.name == name and character.health == health and character.armorClass == armor_class and character.is_player == is_player:
@@ -74,6 +87,9 @@ def remove_selected():
                 creatures.remove(character)
             elif character in players:
                 players.remove(character)
+
+            #we only want to delete one item (case where two creatures have the same amount of health left and name, AC)
+            break   
 
     return redirect(url_for('index'))
 
